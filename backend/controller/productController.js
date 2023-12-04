0fb4c40118f841e034509a5a97dbb980304bfa79
Amarjeet <kumaramarjeet001@gmail.com>
2023-12-04 22:10:27 +0530

    * Product CRUD*

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-//  Create new product
+//  Create new product 
 exports.createProduct =  async (req,res,next)=>{
   const product = await Product.create(req.body) ;
   res.status(201).json({
@@ -10,9 +10,48 @@ exports.createProduct =  async (req,res,next)=>{
 }
 
 
-//  Create a dummy response for get product
-//   const getAllProducts = ((req,res)=>{
-//     res.status(200).json({message:"This is the valid request"})
-// })
+//  Get all product
+exports.getAllProducts = async(req,res)=>{
+    const products = await Product.find() ;
+    res.status(200).json({
+      success:true,
+      products
+    })
+}
 
-// module.exports = createProduct ;
+//  Update product
+exports.updateProduct = async(req,res)=>{
+    let product = await Product.findById(req.params.id) ;
+    if(!product)
+    {
+      res.status(500).json({
+        success:false,
+        message : "Error while getting product"
+      })
+    }
+
+  product = await   Product.findByIdAndUpdate(req.params.id,req.body) ;
+
+    res.status(200).json({
+      succes:true,
+      product
+    })
+}
+
+//  delete product
+exports.deleteProduct = async(req,res)=>{
+  const product = await Product.findById(req.params.id) ;
+  if(!product)
+  {
+   return  res.status(500).json({
+      success:false,
+      message : "Error while getting product"
+    })
+  }
+
+   await product.deleteOne() ;
+  return res.status(200).json({
+    success:true,
+    message : "Product deleted succesfully"
+  })
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express") ;
-const  {createProduct}  = require("../controller/productController");
+const  {createProduct,getAllProducts,updateProduct, deleteProduct}  = require("../controller/productController");
 const router = express.Router() ;
 
-//  Create a route with success response
-// router.route("/products").get(getAllProducts);
+//  Get product with success response
+ router.route("/products").get(getAllProducts);
 //  Route to create new product
 router.route("/product/new").post(createProduct);
-
+//  Route to update product
+router.route("/product/update/:id").post(updateProduct);
+//  Route to delete product
+router.route("/product/delete/:id").delete(deleteProduct);
 module.exports = router ;
