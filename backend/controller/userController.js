5a58d63806bbe756d6b81b884da2a37ab9cda982
Amarjeet <kumaramarjeet001@gmail.com>
2023-12-06 21:53:52 +0530

    * Create jwt token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,6 +2,8 @@ const User = require("../models/userModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
 
+
+
 exports.registerUser = catchAsyncErrors( async (req,res,next)=>{
     const {name,email,password} = req.body ;
     const user = await User.create({
@@ -14,8 +16,10 @@ exports.registerUser = catchAsyncErrors( async (req,res,next)=>{
         }
     }) ;
 
+    const token = user.getJWTToken() ;
+
     res.status(201).json({
         success:true,
-        user,
+        token,
     }) ;
 })
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose") ;
 const validator = require("validator") ;
 const bcrypt = require("bcryptjs") ;
+const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -42,6 +43,7 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpire : Date
 }) ;
 
+//  Saving password in hash
 userSchema.pre("save",async function(next){
     if(!this.isModified("password"))
     {
@@ -49,5 +51,13 @@ userSchema.pre("save",async function(next){
     }
     this.password = await bcrypt.hash(this.password,10) ;
 })
+
+//  JWT Token
+userSchema.methods.getJWTToken = function () {
+    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_EXPIRE,
+    })
+}
+
 
 module.exports = mongoose.model("User",userSchema) ;
