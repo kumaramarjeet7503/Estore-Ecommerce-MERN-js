2d7bb413f59382ec51ec1842ce5c6d6bc00fa5d5
Amarjeet <kumaramarjeet001@gmail.com>
2023-12-07 20:47:32 +0530

    * Authentication for Admin via API

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -8,11 +8,12 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 //  Register user function
 
 exports.registerUser = catchAsyncErrors( async (req,res,next)=>{
-    const {name,email,password} = req.body ;
+    const {name,email,password,role} = req.body ;
     const user = await User.create({
         name,
         email,
         password,
+        role,
         avatar:{
             public_id: "This is the sample id",
             url : "www.example.com"
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -14,3 +14,14 @@ exports.isAuthenticatedUser = catchAsyncError( async(req,res,next)=>{
     req.user =  await User.findById(decodedData.id) ; 
     next() ;
 } )
+
+exports.authorizeRoles = (...roles)=>{
+    return (req,res,next) =>{
+    if(!roles.includes(req.user.role)){
+        return next(new ErrorHandler(`Role: ${req.user.role} is not allowed `,403)) ;
+    }
+    next() ; 
+}
+
+   
+}
