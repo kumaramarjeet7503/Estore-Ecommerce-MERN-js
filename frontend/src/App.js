0e2a54db95ed81aa34654d7c81ba5ae8a105b463
Amarjeet <kumaramarjeet001@gmail.com>
2023-12-14 21:09:40 +0530

    * Header Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.css';
-import Header from  './component/Header.js' ;
+import Header from  './component/layout/Header/Header.js' ;
 import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from './component/layout/Footer/Footer.js';
 
 
 function App() {
   return (
   <Router>
     <Header />
+    <Footer />
     </Router>
   );
 }
